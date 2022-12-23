613b0c1dfc5a71159695390626050966588a3d66
David Miró Bonora <davidbonora23@gmail.com>
2022-12-23 15:17:12 +0100

    start

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima asperiores exercitationem ad enim cumque quia aut officiis unde excepturi aliquam sunt, alias eum numquam maiores dolore eius non sit eaque aspernatur, impedit modi magni ipsam, facere ut! Ab, sapiente nostrum aspernatur numquam ducimus, sequi eaque amet explicabo perferendis tempora animi.</p>
+    </div>
+  );
+}
+
+export default App;
