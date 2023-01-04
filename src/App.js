561c1a0bcc870e6dbaeefba5c8f30a88c5a8b35f
Helen <elena-lopezmoreno@hotmail.com>
2023-01-04 19:56:47 +0100

    carousel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-// import './App.css';
+import "./App.css";
+import Cardsprincipal from "./components/Cardsprincipal";
+
 
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima asperiores exercitationem ad enim cumque quia aut officiis unde excepturi aliquam sunt, alias eum numquam maiores dolore eius non sit eaque aspernatur, impedit modi magni ipsam, facere ut! Ab, sapiente nostrum aspernatur numquam ducimus, sequi eaque amet explicabo perferendis tempora animi.</p>
-    </div>
+<Cardsprincipal/>
   );
 }
 
