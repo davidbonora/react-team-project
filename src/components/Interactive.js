233ec745f313fa08190dd0cd0ec6460356287fbc
laura <carkek@hotmail.com>
2023-01-04 17:53:12 +0100

    completo

--- src/components/Interactive.js
+++ src/components/Interactive.js
@@ -1,11 +1,22 @@
 import CardPeque from "./CardPeque";
 import classes from "./Interactive.module.css";
+import { useState } from "react";
 
 function Interactive() {
+  //   const [active, setActive] = useState(false);
+
+  //   const focusHandle = (e) => {
+  //     // console.log(e.target);
+  //     setActive(true);
+  //     console.log(active);
+  //   };
   return (
     <>
       <h3 className={classes["titulo-principal"]}>
         ALL OF YOUR STAR WARS FAVORITES NOW STREMING ON DISNEY+
+      </h3>
+      <h3 className={classes["titulo-principal-mobile"]}>
+        STREAM STAR WARS ON DISNEY+
       </h3>
       <div className={classes["franja"]}>
         <h2 className={classes["titulo-imagen-uno"]}>
@@ -22,8 +33,10 @@ function Interactive() {
             </div>
             <div className={classes["contenedor-subtitulo-card"]}>
               <div className={classes["contenedor-logo"]}>
-                <div className={classes["logo-linea"]}></div>
-                <div className={classes["logo-circulo"]}></div>
+                <div
+                  className={classes["logo-linea"]}
+                ></div>
+                <div className={`${classes["logo-circulo"]}`}></div>
               </div>
               <span>
                 Star Wars:Tales from the Galaxy's Edge-Enhanced Edition
